24a7926db802d87f5c2ccfe619729e7a25e32420
Alan Treadway <alan@alantreadway.net>
2018-02-10 09:40:15 +0000

    Add Firebase deployment support

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyDCCrkLTTbd6CpEbdLjRtesRIvJod3UpRc',
+    authDomain: 'miner-stats-dev.firebaseapp.com',
+    databaseURL: 'https://miner-stats-dev.firebaseio.com',
+    messagingSenderId: '568136785010',
+    projectId: 'miner-stats-dev',
+    storageBucket: 'miner-stats-dev.appspot.com',
+  },
   production: true,
 };
